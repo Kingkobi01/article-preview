a886989e7f7131f77add0a362a3a09095a94ece0
Kwabena Boakye <kwaboakye93@gmail.com>
2024-05-07 12:56:44 +0000

    chore: Refactor Share component CSS classes for better readability and maintainability

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -12,7 +12,7 @@ const Share: FC<ShareProps> = ({ setIsShareOpen, isShareOpen }) => {
     <div
       className={
         isShareOpen
-          ? `opacity-100 border md:top-0 md:after:bottom-0 md:left-1/2 md:after:left-1/2 md:absolute md:after:absolute flex justify-between md:justify-between items-center md:gap-3 md:after:border-[0.75rem] bg-neutral-400 px-[2.375rem] py-4 md:after:border-t-neutral-400 md:after:border-transparent rounded-b-lg md:rounded-xl md:after:w-[0.75rem] text-neutral-100 md:translate-x-10 md:-translate-y-full md:after:aspect-square md:after:-translate-x-1/2 md:after:translate-y-full duration-200`
+          ? `opacity-100  md:top-0 md:after:bottom-0 md:left-1/2 md:after:left-1/2 md:absolute md:after:absolute flex justify-between md:justify-between items-center md:gap-3 md:after:border-[0.75rem] bg-neutral-400 px-[2.375rem] py-4 md:after:border-t-neutral-400 md:after:border-transparent rounded-b-lg md:rounded-xl md:after:w-[0.75rem] text-neutral-100 md:translate-x-10 md:-translate-y-full md:after:aspect-square md:after:-translate-x-1/2 md:after:translate-y-full duration-200`
           : `hidden`
       }
     >
